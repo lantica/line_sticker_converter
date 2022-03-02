40727f707b3fcefd24fd17f9cf7c7b3fa1ccb660
Sariel <lucifer0702@hotmail.com>
2022-03-02 21:03:15 +0800

    add husky commit hook

--- convert.js
+++ convert.js
@@ -1,43 +1,49 @@
-import sharp from 'sharp';
-import fs from 'fs';
-import path from 'path';
-import unzipper from 'unzipper';
-import axios from 'axios';
-import os from 'os';
-import { pipeline } from 'stream/promises';
+import sharp from "sharp";
+import fs from "fs";
+import { mkdir } from "fs/promises";
+import path from "path";
+import unzipper from "unzipper";
+import axios from "axios";
+import os from "os";
+import { Transform } from "stream";
 
-const regex = /^[0-9]{1,}@2x.png$/g;
+const nameRegex = /^[0-9]{1,}@2x.png$/g;
 
 if (process.argv.length < 3) throw new Error("No path is provided");
 const packId = process.argv[2];
 if (!/^[0-9]{1,}/g.test(packId)) throw new Error("Invalid pack id");
 const url = `https://stickershop.line-scdn.net/stickershop/v1/product/${packId}/iphone/stickers@2x.zip`;
-const resp = await axios({
-    method: 'get',
+const { data } = await axios({
+    method: "get",
     url,
-    responseType: 'stream'
+    responseType: "stream",
 });
 
-const unzippedPath = path.resolve(os.tmpdir(), "sticker");
-await pipeline(
-  resp.data,
-  unzipper.Extract({ path: unzippedPath })  
-);
+const outputPath = process.argv[3] ?? process.cwd();
+await mkdir(path.resolve(outputPath), { recursive: true });
 
-const files = await fs.promises.readdir(unzippedPath);
-const stickers = files.filter(file => regex.test(file));
-console.log(`There are ${stickers.length} stickers in the sticker pack`);
-console.log("Going to convert");
-const outputPath = (process.argv.length > 3) ? process.argv[3] : process.cwd();;
-stickers.forEach(async sticker => {
-    const im = await sharp(path.resolve(unzippedPath, sticker))
-        .resize(512, 512, { fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
-        .toBuffer()
-    const filePath = path.resolve(outputPath, `${path.parse(sticker).name}_512.png`);
-    await sharp(im)
+const stickerResizerFactory = () =>
+    sharp()
+        .resize(512, 512, { fit: "contain", background: { r: 0, g: 0, b: 0, alpha: 0 } })
         .sharpen()
-        .png()
-        .toFile(filePath)
-});
+        .png();
 
-
+data.pipe(unzipper.Parse())
+    .pipe(
+        new Transform({
+            objectMode: true,
+            transform: (entry, e, cb) => {
+                if (nameRegex.test(entry.path)) {
+                    entry
+                        .pipe(stickerResizerFactory())
+                        .pipe(fs.createWriteStream(path.resolve(outputPath, `${path.parse(entry.path).name}_512.png`)))
+                        .on("finish", cb);
+                } else {
+                    entry.autodrain();
+                    cb();
+                }
+            },
+        })
+    )
+    .on("error", (e) => console.error(e))
+    .on("finish", () => console.log("Done!"));
